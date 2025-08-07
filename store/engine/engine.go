60ca35ccde588a62510a5f4bb1291a429754a22e
Ashish Kushwaha <ashish@wishfy.club>
2025-08-07 17:19:15 +0530

    get engine by id

--- store/engine/engine.go
+++ store/engine/engine.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"github.com/geekAshish/DriveDesk/models"
 )
@@ -15,19 +16,40 @@ func New(db *sql.DB) *EnginStore {
 	return &EnginStore{db: db}
 }
 
-func (s EnginStore) GetEngineById(ctx context.Context, id string) (models.Engine, error) {
+func (e EnginStore) GetEngineById(ctx context.Context, id string) (models.Engine, error) {
+	var engine models.Engine
 
+	// begin the transaction , atomic [if we have any error in the middle, we will rollback]
+	tx, err := e.db.BeginTx(ctx, nil)
+	if err != nil {
+		return engine, err
+	}
+
+	defer func() {
+		if err != nil {
+			if rbErr := tx.Rollback(); rbErr != nil {
+				fmt.Printf("transaction rollback error: %v/n", rbErr) 
+			}
+		} else {
+			if cmErr := tx.Commit(); cmErr != nil {
+				fmt.Printf("transaction commit error: %v/n", cmErr) 
+			}
+		}
+
+		err = tx.Commit();
+	}()
+
+	return engine, nil;
 }
 
-func (s EnginStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
+func (e EnginStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
 	
 }
 
-func (s EnginStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
+func (e EnginStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
 
 }
 
-func (s EnginStore) DeleteEngine(ctx context.Context, id string) (models.Car, error) {
+func (e EnginStore) DeleteEngine(ctx context.Context, id string) (models.Engine, error) {
 	
 }
- 
