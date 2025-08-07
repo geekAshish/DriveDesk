8e935b6814d9283d0881e993ce652e6504f6c309
Ashish Kushwaha <ashish@wishfy.club>
2025-08-07 20:38:38 +0530

    create engine

--- store/engine/engine.go
+++ store/engine/engine.go
@@ -3,9 +3,11 @@ package engine
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/geekAshish/DriveDesk/models"
+	"github.com/google/uuid"
 )
 
 type EnginStore struct {
@@ -28,22 +30,74 @@ func (e EnginStore) GetEngineById(ctx context.Context, id string) (models.Engine
 	defer func() {
 		if err != nil {
 			if rbErr := tx.Rollback(); rbErr != nil {
-				fmt.Printf("transaction rollback error: %v/n", rbErr) 
+				fmt.Printf("transaction rollback error: %v/n", rbErr)
 			}
 		} else {
 			if cmErr := tx.Commit(); cmErr != nil {
-				fmt.Printf("transaction commit error: %v/n", cmErr) 
+				fmt.Printf("transaction commit error: %v/n", cmErr)
 			}
 		}
 
-		err = tx.Commit();
+		err = tx.Commit()
 	}()
 
-	return engine, nil;
+	err = tx.QueryRowContext(ctx, `SELECT id, displacement, no_of_cyclinder, car_range FROM engine WHERE id=$1`, id).Scan(
+		&engine.EngineID,
+		&engine.Dispacement,
+		&engine.NoOfCylinders,
+		&engine.CarRange,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return engine, errors.New("engine with id not found")
+		}
+		return engine, err
+	}
+
+	return engine, nil
 }
 
 func (e EnginStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
-	
+	tx, err := e.db.BeginTx(ctx, nil)
+	if err != nil {
+		return models.Engine{}, err
+	}
+
+	defer func() {
+		if err != nil {
+			if rbErr := tx.Rollback(); rbErr != nil {
+				fmt.Printf("transaction rollback error: %v/n", rbErr)
+			}
+		} else {
+			if cmErr := tx.Commit(); cmErr != nil {
+				fmt.Printf("transaction commit error: %v/n", cmErr)
+			}
+		}
+
+		err = tx.Commit()
+	}()
+
+	engineID := uuid.New()
+
+	_, err = tx.ExecContext(
+		ctx,
+		`INSERT INTO engine (id, displacement, no_of_cyclinder, car_range) VALUES ($1, $2, $3, $4)`,
+		engineID, engineReq.Dispacement, engineReq.NoOfCylinders, engineReq.CarRange,
+	)
+
+	if err != nil {
+		return models.Engine{}, nil
+	}
+
+	engine := models.Engine{
+		EngineID:      engineID,
+		Dispacement:   engineReq.Dispacement,
+		NoOfCylinders: engineReq.NoOfCylinders,
+		CarRange:      engineReq.CarRange,
+	}
+
+	return engine, nil
 }
 
 func (e EnginStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
@@ -51,5 +105,5 @@ func (e EnginStore) UpdateEngine(ctx context.Context, id string, engineReq *mode
 }
 
 func (e EnginStore) DeleteEngine(ctx context.Context, id string) (models.Engine, error) {
-	
+
 }
