ef29430f97d563f704bbf769248d18ac21700875
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 15:45:39 +0530

    database connection setup

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,9 +1,14 @@
 package driver
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"time"
 )
+
+var db *sql.DB
 
 func InitDB() {
 	connStr := fmt.Sprintf(
@@ -15,5 +20,28 @@ func InitDB() {
 		os.Getenv("DB_NAME"),
 	)
 
-	
+	fmt.Println("waiting for database connection")
+	time.Sleep(5 * time.Second)
+
+	db, err := sql.Open("postgress", connStr)
+	if err != nil {
+		log.Fatalf("ERROR OPENING DATABASE : %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("ERROR CONNECTING TO THE DATABASE : %v", err)
+	}
+
+	fmt.Println("Successfully connected to database")
+}
+
+func GetDB() *sql.DB {
+	return db
+}
+
+func CloseDB() {
+	if err := db.Close(); err != nil {
+		log.Fatalf("Error closing the database : %v", err)
+	}
 }
