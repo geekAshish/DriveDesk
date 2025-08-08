b3960e34c0bdc4c9a9fe4cc256a72f7963262588
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 16:12:03 +0530

    router setup

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/geekAshish/DriveDesk/driver"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 
-// 10:15
+	carService "github.com/geekAshish/DriveDesk/service/car"
+	carStore "github.com/geekAshish/DriveDesk/store/car"
 
+	engineService "github.com/geekAshish/DriveDesk/service/engine"
+	engineStore "github.com/geekAshish/DriveDesk/store/engine"
 
+	carHandler "github.com/geekAshish/DriveDesk/handler/car"
+	engineHandler "github.com/geekAshish/DriveDesk/handler/engine"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	driver.InitDB()
+	defer driver.CloseDB()
+
+	db := driver.GetDB()
+	carStore := carStore.New(db)
+	carService := carService.NewCarService(carStore)
+
+	engineStore := engineStore.New(db)
+	engineService := engineService.NewEngineService(engineStore)
+
+	carHandler := carHandler.NewCarHandler(carService)
+	engineHandler := engineHandler.NewEngineHandler(engineService)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/cars/{id}", carHandler.GetCarById).Methods("GET")
+	router.HandleFunc("/cars", carHandler.GetCarByBrand).Methods("GET")
+	router.HandleFunc("/cars", carHandler.CreateCar).Methods("POST")
+	router.HandleFunc("/cars/{id}", carHandler.UpdateCar).Methods("PUT")
+	router.HandleFunc("/cars/{id}", carHandler.DeleteCar).Methods("DELETE")
+
+	router.HandleFunc("/engine/{id}", engineHandler.GetEngineById).Methods("GET")
+	router.HandleFunc("/engine", engineHandler.CreateEngine).Methods("POST")
+	router.HandleFunc("/engine/{id}", engineHandler.UpdateEngine).Methods("PUT")
+	router.HandleFunc("/engine/{id}", engineHandler.DeleteEngine).Methods("DELETE")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("INVALID PORT NUMBER")
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("Server listning on : %s", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
+}
