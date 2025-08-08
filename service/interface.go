4de6ac6645b8122a4c2c394fab10162755f6a3d4
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 12:02:07 +0530

    car service inteface

--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,16 @@
 package service
+
+import (
+	"context"
+
+	"github.com/geekAshish/DriveDesk/models"
+)
+
+type CarServiceInterface interface {
+	GetCarById(ctx context.Context, id string) (*models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, car *models.CarRequest) (*models.Car, error)
+	UpdateCar(ctx context.Context, id string, car *models.CarRequest) (*models.Car, error)
+	DeleteCar(ctx context.Context, id string) (*models.Car, error)
+}
+
