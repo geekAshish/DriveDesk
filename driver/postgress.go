b99da394101b4191963cdee1cdff9a32af398eda
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 15:33:28 +0530

    add DB env

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,1 +1,19 @@
 package driver
+
+import (
+	"fmt"
+	"os"
+)
+
+func InitDB() {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	
+}
