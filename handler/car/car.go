96030962d1f446827f2a05fca07a378f0dcdaba4
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 14:16:14 +0530

    get car by brand

--- handler/car/car.go
+++ handler/car/car.go
@@ -47,3 +47,32 @@ func (h *CarHandler) GetCarById(w http.ResponseWriter, r *http.Request) {
 		log.Println("ERROR: ", err)
 	}
 }
+
+func (h *CarHandler) GetCarByBrand(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context();
+	brand := r.URL.Query().Get("brand")
+	isEngine := r.URL.Query().Get("isEngine") == "true"
+
+	res, err := h.service.GetCarByBrand(ctx, brand, isEngine);
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR: ", err)
+		return
+	}
+
+	body, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "applilcation/json");
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(body);
+	if err != nil {
+		log.Println("ERROR: ", err)
+	}
+}
