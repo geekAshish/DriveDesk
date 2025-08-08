340a602934e3cf47d1f5fb50f5d23daeea0ee2b0
Ashish Kushwaha <ashish@wishfy.club>
2025-08-08 15:02:18 +0530

    create engine

--- handler/engine/engine.go
+++ handler/engine/engine.go
@@ -1,1 +1,50 @@
 package engine
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/geekAshish/DriveDesk/service"
+	"github.com/gorilla/mux"
+)
+
+type EngineHandler struct {
+	service service.EngineServiceInterface
+}
+
+func NewEngineHandler(service service.EngineServiceInterface) *EngineHandler {
+	return &EngineHandler{
+		service: service,
+	}
+}
+
+func (h *EngineHandler) GetEngineById(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	res, err := h.service.GetEngineById(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR: ", err)
+		return
+	}
+
+	body, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "applilcation/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("ERROR: ", err)
+	} 
+}
